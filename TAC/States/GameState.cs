4cae29cd998e6528c7cee0942d648c19fe3190cc
Matthew Bland <MEBland8141@muleriders.saumag.edu>
2021-10-19 17:15:02 -0500

    Add project files.

---
+++ TAC/States/GameState.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace The_Alpha_Chronicles.States
+{
+    public class GameState : State
+    {
+        
+        private Player player;
+        private Map map;
+
+        public GameState()
+        {
+            map = new Map();
+            player = new Player();
+        }
+
+        public override void tick()
+        {
+            map.tick();
+            player.tick();
+        }
+
+        public override void render(SpriteBatch spriteBatch)
+        {
+            map.render(spriteBatch);
+            player.render(spriteBatch);
+        }
+    }
+}
---
+++ TAC/States/State.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace The_Alpha_Chronicles.States
+{
+    public abstract class State
+    {
+        public State()
+        {
+            
+        }
+
+        public abstract void tick();
+        public abstract void render(SpriteBatch spriteBatch);
+        
+    }
+}
